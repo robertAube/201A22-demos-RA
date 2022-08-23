6732f4ce70afa512cf1d219a50897c329ebba218
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-08-23 15:47:53 -0400

    Cours #1 G1

---
+++ 201A22G1/PremierProjet/src/PremierPgm.java
@@ -0,0 +1,15 @@
+public class PremierPgm {
+
+    // main
+    public static void main(String[] args) {
+        System.out.println("Bonjour le monde1"); //affiche la chaine de caractères Bonjour le monde
+        System.out.println("Bonjour le monde2"); //affiche la chaine de caractères Bonjour le monde
+        System.out.println("Bonjour le monde3"); //affiche la chaine de caractères Bonjour le monde
+        System.out.println("Bonjour le monde4"); //affiche la chaine de caractères Bonjour le monde
+        System.out.println("Bonjour le monde5"); //affiche la chaine de caractères Bonjour le monde
+        System.out.println("Bonjour le monde6"); //affiche la chaine de caractères Bonjour le monde
+        System.out.println("Bonjour le monde6"); //affiche la chaine de caractères Bonjour le monde
+    }
+}
+
+
