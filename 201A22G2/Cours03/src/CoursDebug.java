aca820b7e3017c5566ad5557f2be538fd9843d74
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-08-29 10:54:54 -0400

    Cours #3 G2

---
+++ 201A22G2/Cours03/src/CoursDebug.java
@@ -0,0 +1,27 @@
+public class CoursDebug {
+    public CoursDebug() {
+        AfficherNbPommmeParPersonne(5,2);
+        AfficherNbPommmeParPersonne(5,1);
+     //   AfficherNbPommmeParPersonne(5,0);
+        AfficherSurfaceRectangle(5, 3);
+    }
+
+    private void AfficherSurfaceRectangle(int longueur, int largeur) {
+        int surface;
+        surface = longueur * largeur;
+
+        System.out.println("Un rectangle dont la largeur est " + largeur + " et la longueur est "+ longueur+" a une surface de " + surface + ".");
+    }
+
+    private void AfficherNbPommmeParPersonne(int nbPomme, int nbPersonne) {
+        float nbPommeParPersonne;
+
+        nbPommeParPersonne = (float) nbPomme / nbPersonne;
+
+        System.out.println("Avec " + nbPomme + " pommes pour " + nbPersonne + " personnes, chacun aura " + nbPommeParPersonne + " pommes.");
+    }
+
+    public static void main(String[] args) {
+        new  CoursDebug();
+    }
+}
