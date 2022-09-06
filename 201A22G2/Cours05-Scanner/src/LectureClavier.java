044bc500ad22f0a71d7e3da7760798c26adf2c05
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-09-06 09:20:07 -0400

    Cours #5 G2 V2

--- 201A22G2/Cours05-Scanner/src/LectureClavier.java
+++ 201A22G2/Cours05-Scanner/src/LectureClavier.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class LectureClavier {
+import java.util.Scanner;
+
+public class LectureClavier {
+    public LectureClavier() {
+//        lireEtAfficherMot();
+
+        jouerAvecOperateurAffectation();
+    }
+
+    private void jouerAvecOperateurAffectation() {
+        int a = 0;
+
+        System.out.println("a = " + a);
+        a += 2; // a = a + 2
+        System.out.println("a = " + a);
+        a -= 1; // a = a - 1
+        System.out.println("a = " + a);
+        a *= 10; // a = a * 10
+        System.out.println("a = " + a);
+        a /= 2; // a = a / 2
+        System.out.println("a = " + a);
+    }
+
+    private void lireEtAfficherMot() {
+        //lire au clavier
+        Scanner scanner; //Instance de l'utilitaire de lecture
+        String mot;
+
+        System.out.print("Entrez un mot : "); //print sans ln pour que la saisie se fasse sur la même ligne
+        scanner = new Scanner(System.in); //Création de l'instance de l'utlitaire de lecture
+
+        mot = scanner.nextLine();
+
+        mot = mot.trim();
+        mot = mot.toUpperCase();
+        mot = mot.replace(" ", "");
+
+        System.out.println("\"" + mot + "\""); // le \ est un caractère de ctrl qui me permet de faire suivre le "
+    }
+
+    public static void main(String[] args) {
+        //Appel du constructeur
+        //Crée une nouvelle instance (objet) de LectureClavier()
+        new LectureClavier();
+
+    }
 }
