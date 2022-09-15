ef8fc3b912427100474c33460cbbf4700ddfd540
Robert <robert.aube@cegeplimoilou.ca>
2022-09-15 07:39:49 -0400

    Cours #6 G1 - 1 projet plusieurs classes

---
+++ 201A22G2/prjDemos201G2/src/Test.java
@@ -0,0 +1,10 @@
+//FORMATIF 3
+//Cours : 420-201 – Introduction à la programmation
+//Groupe : 1
+//Nom : Aubé
+//Prénom : Robert
+//DA : 1234567
+//Date : (2022-09-13)
+package PACKAGE_NAME;public class Test {
+
+}
