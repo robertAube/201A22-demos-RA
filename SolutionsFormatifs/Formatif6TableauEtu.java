67287bc6c9b327585e04b214dc22bc59d5b79a2e
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-10-31 18:21:54 -0400

    MAJ Ajout des solutions de formatif

--- SolutionsFormatifs/Formatif6TableauEtu.java
+++ SolutionsFormatifs/Formatif6TableauEtu.java
@@ -1,13 +1,17 @@
-public class Formatif6Tableau {
-    public Formatif6Tableau() {
-//        trace();
-//        testerGetNbValeursPositives();
-//        testerTrouverIndice();
-//        testerSontDesTableauxEgaux();
-//        testerRemplacerValeur();
+/**
+ * @author Robert Aubé
+ * @version 1.0 (Date de création: 2022-10-31)
+ */
+public class Formatif6TableauEtu {
+    public Formatif6TableauEtu() {
+        trace();
+        testerGetNbValeursPositives();
+        testerTrouverIndice();
+        testerSontDesTableauxEgaux();
+        testerRemplacerValeur();
         testerSortirLesNombresPositifs();
+        testerFindLargestNbs();
     }
-
 
     // 1 //////////////////////////////////////////////////////////////////
     private int[] traceTableau(int t1[]) {
@@ -29,7 +33,7 @@ public class Formatif6Tableau {
     }
 
     private void trace() {
-        int tA[] = {9, -3, 0, 2, 17};
+        int tA[] = { 9, -3, 0, 2, 17 };
         int tB[];
 
         tB = traceTableau(tA);
@@ -43,12 +47,14 @@ public class Formatif6Tableau {
             System.out.print(tB[i] + ", ");
         }
     }
+
     // 4 //////////////////////////////////////////////////////////////////
     private void testerGetNbValeursPositives() {
-        int t1[] = {1, -1, 0, 7};
+
+        int t1[] = { 1, -1, 0, 7 };
         int tVide[] = {};
 
-        System.out.println("testerGetNbValeursPositives");
+        System.out.println("\ntesterGetNbValeursPositives()");
         System.out.println(getNbValeursPositives(t1) == 3);
         System.out.println(getNbValeursPositives(tVide) == 0);
     }
@@ -64,10 +70,10 @@ public class Formatif6Tableau {
 
     // 5 //////////////////////////////////////////////////////////////////
     private void testerTrouverIndice() {
-        int t1[] = {1, -1, 1, 7};
+        int t1[] = { 1, -1, 1, 7 };
         int tVide[] = {};
 
-        System.out.println("testerTrouverIndice");
+        System.out.println("testerTrouverIndice()");
         System.out.println(trouverIndice(t1, -1) == 1);
         System.out.println(trouverIndice(t1, 1) == 0);
         System.out.println(trouverIndice(t1, 7) == 3);
@@ -90,13 +96,13 @@ public class Formatif6Tableau {
 
     // 6 //////////////////////////////////////////////////////////////////
     private void testerSontDesTableauxEgaux() {
-        int t1[] = {1, -1, 2, 7};
-        int t2[] = {1, -1, 2, 7};
-        int t3[] = {1, -1, 2, 8};
-        int t4[] = {1, -1};
+        int t1[] = { 1, -1, 2, 7 };
+        int t2[] = { 1, -1, 2, 7 };
+        int t3[] = { 1, -1, 2, 8 };
+        int t4[] = { 1, -1 };
         int tVide[] = {};
 
-        System.out.println("testerSontDesTableauxEgaux");
+        System.out.println("testerSontDesTableauxEgaux()");
         System.out.println(sontDesTableauxEgaux(t1, t2) == true);
         System.out.println(sontDesTableauxEgaux(t1, t3) == false);
         System.out.println(sontDesTableauxEgaux(t1, t2) == true);
@@ -121,13 +127,13 @@ public class Formatif6Tableau {
 
     // 7 //////////////////////////////////////////////////////////////////
     private void testerRemplacerValeur() {
-        int t1[] = {1, -1, 2, 7, 2};
-        int t2[] = {1, -1, 3, 7, 3};
-        int t3[] = {1, -1, 3, 7, 3};
-        int t4[] = {1, -1};
+        int t1[] = { 1, -1, 2, 7, 2 };
+        int t2[] = { 1, -1, 3, 7, 3 };
+        int t3[] = { 1, -1, 3, 7, 3 };
+        int t4[] = { 1, -1 };
         int tVide[] = {};
 
-        System.out.println("testerRemplacerValeur");
+        System.out.println("testerRemplacerValeur()");
         System.out.println(sontDesTableauxEgaux(t1, t2) == false);
         remplacerValeur(t1, 2, 3);
         System.out.println(sontDesTableauxEgaux(t1, t2) == true);
@@ -139,7 +145,6 @@ public class Formatif6Tableau {
         System.out.println(sontDesTableauxEgaux(tVide, tVide) == true);
     }
 
-
     private void remplacerValeur(int[] tableau, int vOld, int vNew) {
         for (int i = 0; i < tableau.length; i++) {
             if (tableau[i] == vOld) {
@@ -148,19 +153,16 @@ public class Formatif6Tableau {
         }
     }
 
-
-
-
     // 8 //////////////////////////////////////////////////////////////////
     private void testerSortirLesNombresPositifs() {
-        int t1[] = {1, -1, 0, 7, -2};
-        int t1Copie[] = {1, -1, 0, 7, -2};
-        int t1Rep[] = {1, 0, 7};
-        int t2[] = {1, 2, 7, 2};
+        int t1[] = { 1, -1, 0, 7, -2 };
+        int t1Copie[] = { 1, -1, 0, 7, -2 };
+        int t1Rep[] = { 1, 0, 7 };
+        int t2[] = { 1, 2, 7, 2 };
         int tVide[] = {};
         int tRep[];
 
-        System.out.println("testerSortirLesNombresPositifs");
+        System.out.println("testerSortirLesNombresPositifs()");
         tRep = sortirLesNombresPositifs(t1);
         System.out.println(sontDesTableauxEgaux(tRep, t1Rep) == true);
         System.out.println(sontDesTableauxEgaux(t1, t1Copie) == true);
@@ -171,8 +173,6 @@ public class Formatif6Tableau {
         tRep = sortirLesNombresPositifs(tVide);
         System.out.println(sontDesTableauxEgaux(tRep, tVide) == true);
     }
-
-
 
     private int[] sortirLesNombresPositifs(int[] tableau) {
         int tabNbPositif[];
@@ -191,10 +191,52 @@ public class Formatif6Tableau {
         }
         return tabNbPositif;
     }
+    // 9 ///////////////////////////////////////////////////////////////////
+
+    private void testerFindLargestNbs() {
+        System.out.println("testerFindLargestNbs()");
+
+        int[][] tab2D1 = {{4, 2, 7, 1}, {20, 70, 40, 90}, {1, 2, 0}};
+        int[][] tab2D2 = {{}, {20, 70, 40, 90}, {1, 2, 0}};
+        int[][] tab2D3 = {{}, {20, 70, 40, 90}, {1, 2, 0}, {-10}};
+        int[] largestNbs;
+
+        largestNbs = findLargestNbs(tab2D1);
+        afficherTableau(largestNbs);
+        largestNbs = findLargestNbs(tab2D2);
+        afficherTableau(largestNbs);
+        largestNbs = findLargestNbs(tab2D3);
+        afficherTableau(largestNbs);
+    }
+
+    private int[] findLargestNbs(int[][] tab2D) {
+        int[] largestNbs = new int[tab2D.length];
+
+        for (int i = 0; i < tab2D.length; i++) {
+            largestNbs[i] = maxVal(tab2D[i]);
+        }
+
+        return largestNbs;
+    }
+
+    private int maxVal(int[] tab) {
+        int max = tab.length == 0 ? 0 : tab[0];
+        for (int i = 1; i < tab.length; i++) {
+            max = max > tab[i] ? max : tab[i];
+        }
+        return max;
+    }
 
 ////////////////////////////////////////////////////////////////////
+    private void afficherTableau(int[] tableau) {
+        System.out.print("{");
+        for (int i = 0; i < tableau.length; i++) {
+            System.out.print((i == 0 ? "" : ", ") + tableau[i]);
+        }
+        System.out.println("}");
+    }
 
     public static void main(String[] args) {
-        new Formatif6Tableau();
+        new Formatif6TableauEtu();
     }
 }
