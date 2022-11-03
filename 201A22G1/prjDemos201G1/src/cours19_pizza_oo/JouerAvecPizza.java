5e73573dae96080bb5629174b1c9f397dd5cc620
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-11-03 16:19:29 -0400

    G1 Cours 19  OO

---
+++ 201A22G1/prjDemos201G1/src/cours19_pizza_oo/JouerAvecPizza.java
@@ -0,0 +1,104 @@
+package cours19_pizza_oo;
+
+public class JouerAvecPizza {
+    public JouerAvecPizza() {
+
+//        testerLesConstantes();
+//        jouer1();
+//        jouer2();
+//        testerSetXYV1();
+//        testerSetXYV2();
+//        testerValidationVitesse();
+
+        testerLaSurchargeMethode();
+
+    }
+
+    private void testerLaSurchargeMethode() {
+        Pizza p1 = new Pizza(0, 0, 2, "Verte");
+
+        System.out.println(p1);
+        p1.setXY(1,1);
+        System.out.println(p1);
+        p1.setXY(new Coordonnee(3,3));
+        System.out.println(p1);
+    }
+
+    private void testerValidationVitesse() {
+        Pizza p1;
+
+        for (int vitesse = 0; vitesse <= 60; vitesse++) {
+            System.out.println("Instancier avec une vitesse de " + vitesse + ": ");
+            p1 = new Pizza(0, 0, vitesse, "verte");
+            System.out.println(p1);
+        }
+        // il n'y a qu'une pizza: la dernière...
+    }
+
+    private void testerSetXYV2() {
+        Pizza p1;
+        int x = 20;
+        int y = 300;
+
+        //   p1 = new Pizza(x,y, 22, "Jaune");
+        System.out.println(Pizza.estValide(x, y));
+
+        if (Pizza.estValide(x, y)) {
+            p1 = new Pizza(x, y, 22, "Jaune");
+        }
+
+
+    }
+
+    private void testerSetXYV1() {
+        Pizza p1 = new Pizza(0, 0, 2, "Verte");
+        Pizza p2 = new Pizza(2, 6, 2, "Jaune");
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+        p2.setXY(0, 1000);
+        System.out.println(p2);
+
+
+    }
+
+    private void testerLesConstantes() {
+        System.out.println(Coordonnee.MAX_XY);
+        System.out.println(Math.PI);
+    }
+
+    private void jouer2() {
+        Pizza[] tabPizza;
+
+        tabPizza = new Pizza[5];
+
+        for (int i = 0; i < tabPizza.length; i++) {
+            tabPizza[i] = new Pizza(i, i + 2, 1, "bleu");
+        }
+
+        for (int i = 0; i < tabPizza.length; i++) {
+            System.out.println(tabPizza[i]);
+        }
+
+        for (int i = 0; i < tabPizza.length; i++) {
+            tabPizza[i].avancerX();
+        }
+
+        for (int i = 0; i < tabPizza.length; i++) {
+            System.out.println(tabPizza[i]);
+        }
+    }
+
+    private void jouer1() {
+        Pizza p1;
+
+        p1 = new Pizza(0, 0, 2, "jaune"); //création d'une instance de la classe Pizza
+
+        System.out.println(p1);
+    }
+
+    public static void main(String[] args) {
+        new JouerAvecPizza();
+    }
+}
---
+++ 201A22G1/prjDemos201G1/src/cours19_pizza_oo/Pizza.java
@@ -0,0 +1,80 @@
+package cours19_pizza_oo;
+
+public class Pizza {
+    //constante: valeur nommée qui ne peut pas changée. Le nom d'une constante est toujours en MAJUSCULE.
+    //static : appartient à la classe: durée de vie infinie!
+    //final: la variable doit être initialisée et ne pourra jamais être changée.
+    public static final int VITESSE_MIN = 0;
+
+    public static final int VITESSE_MAX = 50;
+    //attributs ou variables d'instance : Existe seulement si j'ai une instance
+    private Coordonnee position;
+    private int vitesse;
+    private String couleur;
+
+    Pizza(int x, int y, int vitesse, String couleur) {
+        position = new Coordonnee();
+        setXY(x, y);
+        setVitesse(vitesse);
+        setCouleur(couleur);
+    }
+
+    //get = accesseur (getter)
+    public int getX() {
+        return position.getX();
+    }
+
+    public int getY() {
+        return position.getY();
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    //mutateurs : c'est les sets (setter)
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public void setVitesse(int vitesse) {
+        if (estValideVitesse(vitesse)) {
+            this.vitesse = vitesse;
+        }
+        else {
+            throw new IllegalArgumentException("Cette vitesse est invalide : " + vitesse);
+        }
+    }
+
+    private static boolean estValideVitesse(int vitesse) {
+        return VITESSE_MIN <= vitesse && vitesse <= VITESSE_MAX;
+    }
+
+    //manque la validation pour faire correctement l'encapsulation
+    public void setXY(int x, int y) {
+        position.move(x,y);
+    }
+
+    //Surcharge : même nom de méthode mais arguments différents en nombre et ou en type
+    public void setXY(Coordonnee position) {
+        this.position = position;
+    }
+
+    public static boolean estValide(int x, int y) {
+        return Coordonnee.estValideX(x) && Coordonnee.estValideY(y);
+    }
+
+    public void avancerX() {
+        position.translate(vitesse, 0);
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "x=" + position.getX() +
+                ", y=" + position.getY() +
+                ", vitesse=" + vitesse +
+                ", couleur='" + couleur + '\'' +
+                '}';
+    }
+}
