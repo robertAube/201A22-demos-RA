d3e8579c65844e8113a764c0dd03d23f18d8bb55
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-11-22 13:41:29 -0500

    G1 Cours 24 - prépration

---
+++ 201A22G1/prjDemos201G1/src/cours24_equals/TestsEquals.java
@@ -0,0 +1,26 @@
+package cours24_equals;
+
+/**
+ * @author Robert Aubé
+ * @version 1.0 (Date de création: 2022-11-20)
+ */
+public class TestsEquals {
+    public TestsEquals() {
+        testsEquals1();
+    }
+
+    private void testsEquals1() {
+        Pizza p1 = new Pizza(1, 1, "rouge", 2);
+        Pizza p2 = new Pizza(1, 1, "rouge", 2);
+        Object o;
+
+        o = p1;
+
+        System.out.println("1- " + (o == p1));
+
+    }
+
+    public static void main(String[] args) {
+        new TestsEquals();
+    }
+}
