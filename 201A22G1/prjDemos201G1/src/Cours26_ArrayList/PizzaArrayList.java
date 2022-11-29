92de00f850b86e22e8a71e6ede1ac003af9926bd
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-11-29 13:21:01 -0500

    G1 - Cours 26 prep

---
+++ 201A22G1/prjDemos201G1/src/Cours26_ArrayList/PizzaArrayList.java
@@ -0,0 +1,9 @@
+package Cours26_ArrayList;
+
+/**
+ * @author Robert Aubé
+ * @version 1.0 (Date de création: 2022-11-29)
+ */
+public class PizzaArrayList {
+
+}
---
+++ 201A22G1/prjDemos201G1/src/Cours26_ArrayList/PizzaArrayListTest.java
@@ -0,0 +1,27 @@
+package Cours26_ArrayList;
+
+/**
+ * @author Robert Aubé
+ * @version 1.0 (Date de création: 2022-11-29)
+ */
+public class PizzaArrayListTest {
+    public PizzaArrayListTest() {
+        ajouterDesPizzas();
+        afficherDesPizzas();
+        avancerDesPizzas();;
+        afficherDesPizzas();
+    }
+
+    private void ajouterDesPizzas() {
+
+    }
+    private void afficherDesPizzas() {
+
+    }
+    private void avancerDesPizzas() {
+
+    }
+    public static void main(String[] args) {
+        new PizzaArrayListTest();
+    }
+}
